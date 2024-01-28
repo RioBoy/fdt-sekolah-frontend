c8b46ea43b1e28b1dcea91e1c17504165d4ce1d3
RioBoy <boyrio68@gmail.com>
2024-01-28 21:49:37 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="container text-teal-500">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. At eveniet
+      assumenda cupiditate aliquid quis ea, officia libero consequatur quod
+      ratione. Asperiores, rem velit? Ut architecto cumque illo provident sed!
+      Libero sit soluta nam deserunt officia est laborum quas, minus repellendus
+      reiciendis nihil quisquam et quae at delectus neque sunt atque.
+    </div>
+  );
+}
+
+export default App;
